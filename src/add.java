49b348546acdb664001ef547977a0c3038552941
sumanalax <sumanalax@gmail.com>
2018-04-14 21:00:46 -0400

    Initial draft of addition.

---
+++ src/add.java
@@ -0,0 +1,16 @@
+public class add {
+public static void main(String[]args){
+
+   int a = 5;
+   int b = 6;
+   int c = a+b;
+       System.out.println(c);
+
+    if(c>20){
+       System.out.println("c is greater" );
+     }
+    if(c<20){
+       System.out.println("c is lesser");
+     }
+   }
+  }
